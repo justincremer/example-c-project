4e502a007750f4b359e3fbb143cb1bff99cca121
jacremer <jacremer@live.com>
2021-08-19 00:56:33 -0700

    simplifies fact

--- src/fact.c
+++ src/fact.c
@@ -2,16 +2,9 @@
 
 int fact(int n)
 {
-    if (n >= -1 && n <= 1) // -1..1
-    {
-        return 1;
-    }
-    else if (n > 0)
-    {
+    if (n > 1)
         return fact(n - 1) * n;
-    }
-    else
-    {
+    if (n < -1)
         return fact(n + 1) * n;
-    }
+    return 1;
 }
